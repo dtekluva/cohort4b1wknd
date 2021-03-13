4c54ef1f371ddf0f436983597cbf7dab932434f1
ValNwangwu <nwangwuv@yahoo.com>
2021-03-13 11:30:38 +0100

    quick merge

---
+++ Dammy_wk2.py
@@ -0,0 +1,125 @@
+# num = 5
+# print(num)
+# type_num = type(num)
+# print(type_num)
+
+
+# ade_decimal = 0.5
+# print(ade_decimal)
+# print(type(ade_decimal)) 
+
+# new_sentence = "This is a weekend class 2!"
+# print(new_sentence)
+# print(type(new_sentence))
+
+# new_sentence = 'This is  Univelcity's weekend class 2!"
+# print(new_sentence)
+# print(type(new_sentence))
+
+# sentence2 = 'Dami said "he just got home"'
+# print(sentence2)
+
+# sent3 = "Dami said\"he just got home\""
+# print(sent3)
+
+# feedback = True
+# print(type(feedback))
+# print(feedback)
+
+
+# new_list = [1,2,3,4,5]
+# print(new_list)
+# print(type(new_list))
+
+# new_list2 = [1,2.0,3, True, "water", 4, 5]
+# print(new_list2)
+# print(type(new_list2))
+
+# new_tuple = ("apple", "pawpaw","orange")
+# print(new_tuple)
+# print(type(new_tuple))
+
+# apple = "mango"
+# new_tuple = (apple, "pawpaw","orange")
+# print(new_tuple)
+# print(type(new_tuple))
+
+# set_of_sports = {"Basketball", "Football", "Boxing", "Swimming"}
+# print(set_of_sports)
+# print(type(set_of_sports))
+
+# personal_info = {
+#     "S/N" : 1, 
+#     "Name" : "Dami",
+#     "Gender": "Male"
+# }
+# print(personal_info)
+# print(type(personal_info))
+
+# personal_info2 = {
+#     "S/N" : [1,2],
+#     "Name" : ["Dami", "Val"],
+#     "Gender" : ("Male", "Male")
+# }
+# print(personal_info2)
+# print(type(personal_info2))
+
+# quick_adder = 5 + 5 + 5
+# print(quick_adder)
+
+# quick_operation = 0.9 - 2.3 + 4 + 18
+# print(quick_operation)
+
+# quick_operation2 = 0.9 - 2.3 + 4 + 18 * 7
+# print(quick_operation2)
+
+# my_modul = 17 % 3
+# print(my_modul)
+
+# my_floordiv = 17 // 3
+# print(my_floordiv)
+
+# score = 5
+# score += 17
+# print(score)
+
+# score2 = 5
+# score2 -= 17
+# print(score2)
+
+# score3 = 5
+# score3 %= 17
+# print(score3)
+
+# first_num = 200
+# second_num = 100 + 100
+# quick_check = first_num == second_num
+# print(quick_check)
+
+# statement1 = 200 == 200
+# statement2 = 100 < 100
+# quick_check = statement1 or statement2
+# print(quick_check)
+
+
+# statement1 = 200 == 200
+# statement2 = 100 < 100
+# quick_check = not(statement1 and statement2)
+# print(quick_check)
+
+# nut_cracker = "This is nuts!"
+# member_check = "This" in nut_cracker
+# print(member_check)
+
+# member_check2 = "this" in nut_cracker
+# print(member_check2)
+
+# my_states = ["Abia", "Zamfara", "Lagos", "Rivers", "Kogi"]
+# member_check3 = "zamfara" in my_states
+# print(member_check3)
+
+ident_one = 45
+ident_two = 60
+identity_check = ident_one is ident_two
+print(identity_check)  
+
