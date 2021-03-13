d213822ff4661f9179598d5c7f6d2adafa4ffad4
ejupoju <ejuayeadepoju@gmail.com>
2021-03-13 15:39:36 +0100

    first commit

---
+++ eju/ejupython.py
@@ -0,0 +1,294 @@
+# num_one = 5
+# print(num_one)
+# #print(type(num_one))
+# ade_decimal = 0.5
+# print(type(ade_decimal))
+# ade_decimal = .5
+# new_sentence = 'This is a weekend class2!'
+# print(new_sentence)
+# print(type(new_sentence))
+#new_sentence = 'This is Univelcity's weekend class2!'
+# new_sentence = 'This is Univelcity\'s weekend class 2!'
+# print(type(new_sentence))
+# new_sentence = "This is a Univelcity's weekend class 2!"
+# print(type(new_sentence))
+#sent2 = "Dami said \"he just got home\""
+
+# feedback = True
+# print(feedback)
+# print(type(feedback))
+
+
+
+# new_list = [1,2.0,True,4,'water']
+# print(new_list)
+# print (type(new_list))
+
+# new_tuple = ("Apple","Strawberry","Watermelon")
+# print(new_tuple)
+# print(type(new_tuple))
+
+# set_of_sports = {"1.0","Football","Boxing","Swimming"}
+# print(set_of_sports)
+# print(type(set_of_sports))
+
+# personal_info = {
+#     "S/N" : [1,2],
+#     "Name" : ["Dami", "Valentine"],
+#     "Gender" : ["Male", "Male"]
+# }
+# print(personal_info)
+# print(type(personal_info))
+
+# quick_adder = 5.5 + 5 + 0.5
+# print(quick_adder)
+
+
+# quick_operation= 0.9 - 2.3 + 4 + 18 * 7 / 2
+# print(quick_operation)
+
+# my_modul = 17 // 3
+# print(my_modul)
+
+
+# score = 5
+# score %= 17
+# print(score)
+
+# first_num = 150
+# second_num = 200 - 50
+# quick_check = first_num == second_num
+# print(quick_check)
+
+# first_num = 100
+# second_num = 150
+# quick_check = first_num != second_num
+# print(quick_check)
+
+
+# first_num = 200
+# second_num = 200
+# quick_check = first_num == second_num
+# print(quick_check = False)
+
+# first_num = 200
+# second_num = 200
+# quick_check = first_num == second_num
+# print(quick_check != False)
+
+# first_num = 205
+# second_num = 200
+# quick_check = first_num >= second_num
+# print(quick_check)
+
+# statement1 = 205 == 205
+# statement2 = 200 < 16
+# quick_check = not(statement1 or statement2)
+# print(quick_check)
+
+# statement1 = 205 == 205
+# statement2 = 200 < 16
+# quick_check = not((statement1 or statement2) and statement1)
+# print(quick_check)
+
+
+# nut_cracker = "This is nuts!"
+# member_check = "This" in nut_cracker
+# print(member_check)
+
+# my_states = "Abia", "Zamfara", "Lagos", "Rivers", "Kogi"
+# member_check2 = "zamfara" not in my_states
+# print(member_check2)
+
+
+# ident_one = 45
+# ident_two = 60
+
+# identity_check = ident_one is ident_two
+# print(identity_check) 
+
+# x=2
+# num_1= x
+# print(num_1)
+# y=5
+# num_2=y
+# print(num_2)
+# equa_1= x * 2 + x ** 5
+# print(equa_1) 
+# equa_2 = 61 ** 0.5 - 18
+# print(equa_2)
+# equa_3 = 4 * x * y ** 6 
+# print(equa_3)
+# equa_4= 4 * y + x * y
+# print(equa_4)
+# ans_1 = equa_1 / equa_2
+# print(ans_1)
+# ans_2 = equa_3 / equa_4
+# print(ans_2)
+# answer = ans_1 / ans_2
+# print(answer)
+
+
+
+# x=2
+# num_1= x
+# print(num_1)
+# y=5
+# num_2=y
+# print(num_2)
+# num_3 = 61**0.5
+# print(num_3)
+# equa_1= x**5 / num_3
+# print(equa_1)
+# equa_2 = 2 * x + equa_1 - 18
+# print(equa_2)
+# equa_3 = 4 * y**6 + x * y
+# print(equa_3)
+
+# ans_1 = equa_2 / equa_3
+# print(ans_1)
+
+
+# -0.0001267398
+
+# x = 2
+# y = 5
+
+# numerator = (2 * x + (x**5 / 61**0.5) - 18)
+# denominator = (4 * y**6) / 4*y + x*y
+
+# print(numerator/denominator)
+
+# x = 2
+# y = 2
+
+# print(x is y)
+# x = 2
+# z = 4/2
+
+# print(x is z)
+# print()
+# print("x=id-" )
+
+
+# get remaining notes from Atha
+
+
+
+
+# message= 'Allow me to enlighten you,This is a new era'
+# print(message)
+# message = 'Allow me to enlighten you,\n This is a new era'
+# print(message)
+# message = 'Allow me to enlighten you,\n\t This is a new era'
+# print(message)
+
+# message= '1,2,3,4,5'
+# preferred_end = '\n'
+# print(message)
+# message= '1,2,3,4,5'
+# preferred_end = '\n\t\n'
+# print(1, end = preferred_end)
+# print(2, end = preferred_end)
+# print(3, end = preferred_end)
+# print(4, end = preferred_end)
+
+# sep = ""
+# print("Kunle","Ada", "Shola")
+# print("Kunle","Ada", "Shola", sep=",")
+# print("Kunle","Ada", "Shola", sep="-")
+# print("Kunle", "Ada", "Shola",  sep=" and ")
+
+# message1 = "This is a message to the people of my lovely country."
+# message2 = "Be strong and fight against injustice"
+
+# file = open("message.txt", "w")
+# print(message1, message2, sep= "\nPARAGRAGH ENDS \n\n", file=file)
+
+
+# csv_file = open("eju/message.csv", "w")
+# print(1,2,3,4,5,68,1, sep= ",", file = csv_file)
+# print(2,3,4,5,6,7,8,9, sep=".", file = csv_file)
+
+
+# message3 = "I am simply fed up of my wealth."
+# message4 = "Said no one ever"
+
+# file = open("message.txt", "w")
+# print(message3, message4, sep= "\nPARAGRAPH ENDS \n\n", file=file)
+
+
+# message3 = "I am simply fed up of my wealth."
+# message4 = "Said no one ever"
+# message5 = "This is not true"
+
+# file = open("message.txt", "w")
+# print(message3, message4, message5, sep= "\nPARAGRAPH ENDS \n\n\t", file=file)
+
+# message3 = "I am simply fed up of my wealth."
+# message4 = "Said no one ever"
+# message5 = "This is not true"
+
+# file = open("message.txt", "a")
+# print(message3, message4, message5, sep= "\nPARAGRAPH ENDS \n\n", file=file)
+
+# csv_file = open("eju/message.csv", "w")
+# print(1,2,3,4,5,68,1, sep= "", file = csv_file)
+# print(2,3,4,5,6,7,8,9, sep=".", file = csv_file)
+
+# import datetime
+
+# print(datetime.datetime.now())
+
+# log_file = open("eju/Logs.csv", 'w')
+
+# print(datetime.datetime.now(), "enter was presed", sep=",")
+
+# number = -20
+
+# print("Original-:",number )
+# print("Abs -", abs(number))
+
+# numbers = [2,4,1,7,2,2,10]
+# print(min(numbers))
+
+# numbers = [2,4,1,7,2,2,10]
+# print(max(numbers))
+
+# print(min("Asholanke"))
+# print(max("Asholanke"))
+
+# name = "Adefarasin Adebayo"
+# print(len(name))
+
+# name = "Adefarasin Adebayo {oti,mary}"
+# print(len(name))
+
+
+# file = open("eju/journal.txt", "r")
+# print(file)
+
+
+
+val ="32"
+print(val * 2)
+casted_val= int(val)
+print(casted_val * 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
