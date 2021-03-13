8db2d938de4c7c56af292bad578fbc6ec92a1888
Eberechi520 <ebereopara22@yahoo.com>
2021-03-13 16:08:49 +0100

    first commit

---
+++ Hey-me/week 2 class.py
@@ -0,0 +1,86 @@
+# num_one = 5
+# print (num_one)
+
+# #print(type(num_one))
+ade_decimal = 266
+Ade = .6
+print (Ade)
+print(ade_decimal)
+
+new_word = " \"I am a girl /..?"
+print(new_word)
+print(type(new_word))
+
+yea = True
+print(yea)
+print (type(yea))
+
+_list = [1,2,3]
+print(type(_list))
+
+
+sum = 5.9 +2
+print(sum)
+
+_tuple = ("apple", "pineapple")
+print(_tuple)
+print(type(_tuple))
+
+
+set_of_names = {"mi", "why", "Emi", "nil"}
+print(set_of_names)
+
+_dictionary = {
+    "s/n":{1,2,3},
+    "name" : "Dami",
+    "Gender" :"Male"
+}
+print(_dictionary)
+print(type(_dictionary))
+
+
+sum = 5.9 + 7
+print (sum)
+print(type(sum))
+
+modulus = 17%3
+print(modulus)
+
+
+
+num = 5
+num /= 17
+print(num)
+
+sec_num = 90
+first_num = 90
+print(sec_num == first_num)
+
+print (sec_num != False)
+
+statement_one = 200==200
+Statement_two = 200 <6
+quickcheck = not(statement_one or Statement_two)
+print(quickcheck)
+
+
+nut = ("this is me")
+check = "this" in nut
+print(check)
+
+nut = ("This is me")
+check =  "this" not in nut
+print(check)
+
+identity_one = 45
+identity_two = 60
+check = identity_one is identity_two
+print (check)
+
+
+
+
+
+
+
+
---
+++ Hey-me/week 3 class.py
@@ -0,0 +1,87 @@
+print (1, end="\n")
+print (1)
+info1 = "this is my class"
+inf02 = "why are you fine"
+print("info1","inf02", end="\n that's all")
+
+
+print("ade","me","you", sep = ",,")
+print("ade","me","you", sep = "/")
+
+message1 = "this is me"
+message2= "thanks"
+file= open("message.txt", "w")
+print(message1,message2,file=file)
+
+message1 = "1,2,3,4"
+message2= "5,6,7,8"
+file= open("excel.csv", "w")
+print(message1,message2,file=file)
+
+# message1 = "1,2,3,4"
+# message2= "5,6,7,8"
+# file= open("excel.csv", "a")
+# print(message1,message2,file=file)
+
+message1 = "1,2,3,4"
+message2= "5,6,7,8"
+file= open("excel.csv", "w")
+print(message1,message2,file=file)
+
+message1 = "1,2,3,4"
+message2= "5,6,887,7uy8"
+file= open("excel.csv", "a")
+print(message1,message2,file=file)
+
+
+# print (1, end="\n")
+# print (1)
+info1 = "this is my class"
+info2 = "why are you fine."
+
+# print(info1, inf02, end="\t that all")
+# print(info1)
+# print()
+# print (inf02)
+
+# print(info1, end = "\n\n")
+# print (inf02)
+
+print(info1, info2, sep="\n")
+# print (inf02)
+# print(info1, info2, end="\n")
+
+x=2
+z=4/2
+print(x==z)
+print(x is z)
+y=5
+numerator = (2*x)+(x**5)/((61**0.5)-18)
+denominaator = (20**6)/(4*y)+(x*y)
+print(numerator/denominaator)
+
+
+import datetime
+print(datetime.datetime.now())
+
+num= [778,657,7647,874]
+# print (abs(num))
+print(min(num))
+
+
+file = open("atha/journal.txt", "r")
+print(file)
+print(file.read())
+
+val = "32"
+casted_val= int(val)
+print(val*2)
+print(casted_val*2)
+
+file = open("Hey-me/jojo.txt", "r")
+print(file)
+print(file.read())
+
+file = open("message.txt", "r")
+print(file)
+print(file.read())
