9ffa164aab343bd9108b546e12f6bb7526a47878
segun asani <segunasa@yahoo.com>
2021-03-13 15:44:36 +0100

    first commit

---
+++ segun/Untitled-1.py
@@ -0,0 +1,33 @@
+
+using System;
+namespace HelloWorldApplication
+{
+    class HelloWorld{
+
+static void Main(String[] args)
+{
+    Console.WriteLine("Hello World")
+    Console.ReadKey()
+    
+}
+    }
+}
+
+using System;
+// Using System.collection.Generic;
+// using System.Linq;
+// Using System.Text;
+
+
+namespace Test
+{
+    class HelloWorld{
+
+static void Main(String[] args)
+{
+    Console.WriteLine("Hello World")
+    Console.ReadKey()
+    
+}
+    }
+}
