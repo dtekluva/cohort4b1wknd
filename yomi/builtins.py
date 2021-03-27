644523c21d4fd914cb653aed1713668402fe016f
yomisanda <yomex81@yahoo.com>
2021-03-27 16:05:28 +0100

    update

--- yomi/builtins.py
+++ yomi/builtins.py
@@ -62,23 +62,68 @@
 # char_count = input("Type Charcter to be counted: ")
 # print(len(char_count))
 
-# Principal = input("Please insert The Principal: ")
-# Rate = input("Please input the rate: ")
-# time = input("Please input the time in years: ")
+# action = input("what will you like to do no?\n\nA: Simple Interest\nB: Candy Crusher\n\n")
 
-# Interest = (int(Principal) * float(Rate) * int(time))/100
-# Total_Payable = int(Principal) + int(Interest)
-# print("The Interest for this transaction is", Interest)
-# print("The Total amount Payable would be", Total_Payable)
+# if action == "A":
+#     Principal = input("Please insert The Principal: ")
+#     Rate = input("Please input the rate: ")
+#     time = input("Please input the time in years: ")
 
-Friend1 = input("How Many sweets did friend 1 bring: ")
-Friend2 = input("How many sweets did friend 2 bring: ")
-Friend3 = input("How many sweets did friend 3 bring: ")
+#     Interest = (int(Principal) * float(Rate) * int(time))/100
+#     Total_Payable = int(Principal) + int(Interest)
+#     print("The Interest for this transaction is", Interest)
+#     print("The Total amount Payable would be", Total_Payable)
 
-perperson = (int(Friend1) + int(Friend2) + int(Friend3))/3
-remainder = (int(Friend1) + int(Friend2) + int(Friend3)) % 3
-import math
-f,i = math.modf(perperson)
-print("Shared equally, each of you should get", int(perperson), "sweets and probably discard", remainder, "to avoid a fight")
-input("Please press enter when done")
+# if action == "B":
+#     Friend1 = input("How Many sweets did friend 1 bring: ")
+#     Friend2 = input("How many sweets did friend 2 bring: ")
+#     Friend3 = input("How many sweets did friend 3 bring: ")
 
+#     perperson = (int(Friend1) + int(Friend2) + int(Friend3))/3
+#     remainder = (int(Friend1) + int(Friend2) + int(Friend3)) % 3
+#     # import math
+#     # f,i = math.modf(perperson)
+#     print("Shared equally, each of you should get", int(perperson), "sweets and probably discard", remainder, "to avoid a fight")
+
+# if action!= "A" or action != "B":
+#     print("INVALID SELECTION")
+
+# x = 5
+# y = 0
+
+# if x < y:
+#     print('yes')
+# else:
+#     print("no")
+
+# val_a = int(input("Please Enter Value A: "))
+# val_b = int(input("Please Enter Value B: "))
+
+# if val_a > val_b:
+#     print(val_a, "is Larger than", val_b)
+# else:
+#     print(val_b,  "is Larger than", val_a)
+
+
+# word = input("Please type your first and last name: ")
+
+# if len(word) < 6:
+
+#     if word.startswith ("A") and word.endswith ("t"):
+#         print("Weldone", word, "starts with 'A' and ends with 't'")
+#     else:
+#         print("Ehya, not today")
+# else:
+#     print("Sorry, one or more criterias not met")
+
+import datetime
+current_time = datetime.datetime.now()
+# print(current_time)
+
+seconds = current_time.second
+print(seconds)
+
+if seconds <= 30:
+    print("First Half")
+else: 
+    print("Second Half")
