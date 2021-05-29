efcb70e8437966157d82ed8c82664bb3b7d83cb8
yomisanda <yomex81@yahoo.com>
2021-05-29 17:32:23 +0100

    UPDATE

--- yomi/first.py
+++ yomi/first.py
@@ -41,11 +41,52 @@
 
 
 
-a= input("Math score: ")
-d = input("English score: ")
-e = input("French score: ")
-b = "Mk scored the following;"
-c = " and his cummulative is;"
-f = int(a) + int(d) + int(e)
-print(b, a, d, e, c, f)
+# a= input("Math score: ")
+# d = input("English score: ")
+# e = input("French score: ")
+# b = "Mk scored the following;"
+# c = " and his cummulative is;"
+# f = int(a) + int(d) + int(e)
+# print(b, a, d, e, c, f)
+
+# a=int(input("sweet set A:"))
+# b=int(input("sweet set B:"))
+# c=int(input("sweet set C:"))
+# Total_sweet=int(a)+int(b)+int(c)
+# Each_person_gets=(Total_sweet//3)  # get floor division
+# sweet_remaining=(Total_sweet%3)  
+# print ("Total sweet=",Total_sweet)
+
+# print ("Each gets=", Each_person_gets)
+# print("Remainder=", sweet_remaining)
+f= 10
+# f+=80
+# print(f)
+# a=[1,5,"girl","python",3.40,"she", "like"]
+# print(1 is not a)
+# y=10
+# z=30
+# # print(f = z)
+
+# a=[2,4,6,8,10]
+# # print(max(a))
+
+# b = " Oyin is a girl"
+# print(b)
+# newb = b.replace("Oyin","Yomi")
+# newb1=newb.replace("girl", "boy")
+# print(newb1) 
+# y= "oyin is a 10 Years Old girl"
+# print (y.swapcase())
+# print(y.title())
+# print(y.capitalize())
+nameof= input("Input Student name: ")
+subject= input("Input student's Subject: ")
+x = int(input("Input student's score to get grade?: "))
+if x>=70:
+    print(f"{nameof} got Grade A in {subject}")
+elif x >=50:
+    print(f"{nameof} got Grade B in {subject}")
+elif x>=30:
+    print(f"{nameof} got Grade C in {subject}")
 
